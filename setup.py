cd133259b58d9b7f5e9f2069b40e83088729f930
Matt Revell <matthew.revell@lonelyplanet.com>
2013-01-14 22:24:02 +0000

    Convert to a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='Temper',
+        version='1.0',
+        description='Python tool for reading temerature',
+        packages=["temper"]
+        )
