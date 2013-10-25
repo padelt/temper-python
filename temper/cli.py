a5c2498ff5439e2c4ee69812033c59ffb656aec8
Joji Monma (GM3D) <gmomma940@gmail.com>
2013-10-25 22:52:04 +0900

    Add bus-port info output to frontend.
    Add -h option to temper/cli.py for usage help.
    Add -p option to temper/cli.py for bus-port output.

--- temper/cli.py
+++ temper/cli.py
@@ -1,9 +1,28 @@
 # encoding: utf-8
 from __future__ import print_function
 from temper import TemperHandler
+import getopt, sys, os.path
 
+def usage():
+    print("%s [-p] [-h|--help]" % os.path.basename(sys.argv[0]))
 
 def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], ":hp", ["help"])
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+        sys.exit(2)
+    disp_ports = False
+    for o, a in opts:
+        if o == "-p":
+            disp_ports = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        else:
+            assert False, "unhandled option"
+
     th = TemperHandler()
     devs = th.get_devices()
     readings = []
@@ -13,10 +32,19 @@ def main():
         readings.append({'device': i,
                          'temperature_c': dev.get_temperature(),
                          'temperature_f':
-                         dev.get_temperature(format="fahrenheit")
+                         dev.get_temperature(format="fahrenheit"),
+                         'ports': dev.get_ports(),
+                         'bus': int(dev._bus.dirname)
                          })
 
     for reading in readings:
-        print('Device #%i: %0.1f°C %0.1f°F' % (reading['device'],
-                                               reading['temperature_c'],
-                                               reading['temperature_f']))
+        if disp_ports:
+            portinfo = " (bus %d - port %s)" % (reading['bus'],
+                                                reading['ports'])
+        else:
+            portinfo = ""
+        print('Device #%i%s: %0.1f°C %0.1f°F' 
+              % (reading['device'],
+                 portinfo,
+                 reading['temperature_c'],
+                 reading['temperature_f']))
