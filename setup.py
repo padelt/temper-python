af7c7ea8ac5a5c333c388d4c9e440c674fa078ed
James Stewart <jstewart101@gmail.com>
2014-02-03 16:14:36 +1100

    added additional fields to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='Temper',
+    name='temperusb',
+    author='Philipp Adelt',
+    author_email='info@philipp.adelt.net ',
+    url='https://github.com/padelt/temper-python',
     version='1.1.1',
     description='Reads temperature from TEMPerV1 devices (USB 0c45:7401)',
-    packages=['temper'],
+    long_description=open('README.md').read(),
+    packages=['temperusb'],
     install_requires=['pyusb'],
     entry_points={
         'console_scripts': [
