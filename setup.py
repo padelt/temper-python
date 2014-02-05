a5b85240a9352516711010553cae18e96f6e1fc4
Philipp Adelt <info@philipp.adelt.net>
2014-02-05 10:57:22 +0100

    Fix console script entries for poll and snmp in package setup.py (closes #10) version bump for package to 1.1.2

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup
 setup(
     name='temperusb',
     author='Philipp Adelt',
-    author_email='info@philipp.adelt.net ',
+    author_email='autosort-github@philipp.adelt.net ',
     url='https://github.com/padelt/temper-python',
-    version='1.1.1',
+    version='1.1.2',
     description='Reads temperature from TEMPerV1 devices (USB 0c45:7401)',
     long_description=open('README.md').read(),
     packages=['temperusb'],
     install_requires=['pyusb'],
     entry_points={
         'console_scripts': [
-            'temper-poll = temper.cli:main',
-            'temper-snmp = temper.snmp:main'
+            'temper-poll = temperusb.cli:main',
+            'temper-snmp = temperusb.snmp:main'
         ]
     }
 )
