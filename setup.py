48e7a8fefff386ae7de2eaf07c7fb52a5f20bc80
amorphic <jstewart101@gmail.com>
2014-05-10 16:02:55 +1000

    Adding pyusb 1.0.0b1 as a dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='Reads temperature from TEMPerV1 devices (USB 0c45:7401)',
     long_description=open('README.md').read(),
     packages=['temperusb'],
-    install_requires=['pyusb'],
+    install_requires=['pyusb>=1.0.0b1'],
     entry_points={
         'console_scripts': [
             'temper-poll = temperusb.cli:main',
