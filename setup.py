f9ff2fff2ce8bdd16b26c76c9db4dd075f7776a3
Philipp Adelt <autosort-github@philipp.adelt.net>
2015-12-22 15:06:29 +0100

    Use pyusb 1.0.0rc1; disable unneeded interaction; no more USBErrors for padelt; simplify temperature calculation

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ setup(
     author='Philipp Adelt',
     author_email='autosort-github@philipp.adelt.net ',
     url='https://github.com/padelt/temper-python',
-    version='1.2.4',
+    version='1.3.0b1',
     description='Reads temperature from TEMPerV1 devices (USB 0c45:7401)',
     long_description=open('README.md').read(),
     packages=['temperusb'],
     install_requires=[
-        'pyusb==1.0.0b1',
+        'pyusb>=1.0.0rc1',
     ],
+    dependency_links = ['git+https://github.com/walac/pyusb.git#egg=pyusb-1.0.0rc1'],
     entry_points={
         'console_scripts': [
             'temper-poll = temperusb.cli:main',
