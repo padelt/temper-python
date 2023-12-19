08cafca21b7b209157dce08bdd1c95b0c8a00188
Dave T <17680170+davet2001@users.noreply.github.com>
2023-12-19 21:05:22 +0000

    Update version info

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Philipp Adelt',
     author_email='autosort-github@philipp.adelt.net ',
     url='https://github.com/padelt/temper-python',
-    version='1.6.0',
+    version='1.6.1',
     description='Reads temperature from TEMPerV1 devices (USB 0c45:7401)',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
@@ -24,7 +24,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
     ],
 )
